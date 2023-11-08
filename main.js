8c900502ba8befe6efabaffdf329f77c43f53af8
Alberto Álvarez <alber.alvagon@alum.uca.es>
2023-11-08 12:49:23 +0100

    Adding Skill Set Section

---
+++ main.js
@@ -0,0 +1,11 @@
+const spellBook = document.getElementById("spellbook");
+const mainContainer = document.getElementById("container");
+
+
+spellBook.addEventListener("click", function () {
+ 
+
+  
+  });
+ 
+
